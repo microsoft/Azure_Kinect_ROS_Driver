647387a906084411f5b662345b5e8fe2d60e930f
Zhuo Chen <zchenpds@gmail.com>
2021-03-24 21:16:26 -0400

    Avoid k4abt tracker queue overflow. Discard capture if queue size >= 3

--- src/k4a_ros_device.cpp
+++ src/k4a_ros_device.cpp
@@ -985,7 +985,7 @@ void K4AROSDevice::framePublisherThread()
 
 #if defined(K4A_BODY_TRACKING)
         // Publish body markers when body tracking is enabled and a depth image is available
-        if (params_.body_tracking_enabled &&
+        if (params_.body_tracking_enabled &&  k4abt_tracker_queue_size_ < 3 &&
             (body_marker_publisher_.getNumSubscribers() > 0 || body_index_map_publisher_.getNumSubscribers() > 0))
         {
           if (!k4abt_tracker_.enqueue_capture(capture))
